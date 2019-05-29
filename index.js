05dab625888f1b0b6e395919e55f4a133e0c252e
tyler knapp <tgknapp11@gmail.com>
2019-05-29 15:31:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ function theBeatlesPlay(musicians, instruments) {
   return anArray;
 }
 
-function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
+function johnLennonFacts(array) {
   var i = 0
-  while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
+  while (i < array.length) {
+    array[i] += '!!!';
     i++;
   }
-  return newFacts;
+  return array;
 }
 
 function iLoveTheBeatles(num) {
